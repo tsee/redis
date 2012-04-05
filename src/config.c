1e26f82c3be03a59aec933698d3b9528e5092004
Steffen Mueller <smueller@cpan.org>
2012-04-05 07:45:02 +0200

    Support for multiple 0MQ endpoints
    Conflicts:
    
    	src/config.c

--- src/config.c
+++ src/config.c
@@ -473,13 +473,17 @@ void loadServerConfigFromString(char *config) {
                 err = sentinelHandleConfiguration(argv+1,argc-1);
                 if (err) goto loaderr;
             }
-        } else if (!strcasecmp(argv[0],"expiry-zmq-endpoint") && argc <= 2) {
-            if (argc == 2) {
-              if (redis_zmq_endpoint)
-                zfree(redis_zmq_endpoint);
-              redis_zmq_endpoint = zstrdup(argv[1]);
-            } else {
-              redis_zmq_endpoint = NULL;
+        } else if (!strcasecmp(argv[0],"expiry-zmq-endpoints")) {
+            unsigned int iendpoint, old_num_endpoints;;
+
+            if (argc > 1) {
+                old_num_endpoints = redis_zmq_num_endpoints;
+                redis_zmq_num_endpoints += argc-1;
+                redis_zmq_endpoints = zrealloc(redis_zmq_endpoints, redis_zmq_num_endpoints * sizeof(char *));
+
+                for (iendpoint = 1; iendpoint <= redis_zmq_num_endpoints; ++iendpoint) {
+                    redis_zmq_endpoints[old_num_endpoints+iendpoint-1] = zstrdup(argv[iendpoint]);
+                }
             }
         } else if (!strcasecmp(argv[0],"expiry-zmq-hwm") && argc == 2) {
             redis_zmq_hwm = atoi(argv[1]);
