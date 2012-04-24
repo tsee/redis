77c11018c790427672fc05de5dba9d1003875d88
Steffen Mueller <smueller@cpan.org>
2012-04-24 12:46:14 +0200

    0MQ expiry message docs

--- utils/zmq_expiry_msg_client.pl
+++ utils/zmq_expiry_msg_client.pl
@@ -2,53 +2,39 @@ use strict;
 use warnings;
 
 use ZeroMQ qw/:all/;
-my $cxt = ZeroMQ::Context->new;
-my $sock = $cxt->socket('SUB');
-$sock->connect("tcp://*:9998");
-
-$| = 1;
-
-$sock->setsockopt(ZMQ_SUBSCRIBE, '');
-
 use constant {
   REDIS_ZMQ_TYPE_STRING => 0,
   REDIS_ZMQ_TYPE_HASH   => 1,
 };
 
+my $cxt = ZeroMQ::Context->new;
+my $sock = $cxt->socket('SUB');
+$sock->connect("tcp://*:9998");
+$sock->setsockopt(ZMQ_SUBSCRIBE, '');
+
+$| = 1;
+
 my $i = 0;
 while(1) {
-  my $hdr = $sock->recv->data;
-  if (not $sock->getsockopt(ZMQ_RCVMORE)) {
-    warn "GOT ONLY ONE MESSAGE FRAME!!!";
-    next;
-  }
-  my $raw_key = $sock->recv->data;
-  if (not $sock->getsockopt(ZMQ_RCVMORE)) {
-    warn "GOT ONLY TWO MESSAGE FRAMES!!!";
-    next;
-  }
-  my $raw_data = $sock->recv->data;
+  my @frames = ($sock->recv->data);
+  push @frames, $sock->recv->data while $sock->getsockopt(ZMQ_RCVMORE);
+  warn("Need 3 message frames, got " . scalar(@frames)), next
+    if @frames != 3;
+  my ($hdr, $raw_key, $raw_value) = @frames;
 
-  next if not defined $raw_data or length($raw_data) == 0;
   my ($dbnum, $type) = unpack('vv', $hdr);
-  my ($k) = unpack("V/A", $raw_key);
-  print "dbnum='$dbnum' type='$type' key='$k'\n";
+  my ($key_str) = unpack("V/A", $raw_key);
+
+  my $value;
   if ($type == REDIS_ZMQ_TYPE_STRING) {
-    my ($v) = unpack("V/A", $raw_data);
-    print "  STRING. value='$v'\n";
+    ($value) = unpack("V/A", $raw_data);
   }
   elsif ($type == REDIS_ZMQ_TYPE_HASH) {
     my %h = unpack("V/(V/A)", $raw_data);
-    print "  HASH.\n";
-    print "    $_ => $h{$_}\n" for keys %h;
+    $value = \%h;
   }
   else {
     warn "  GOT UNKNOWN DATA TYPE!";
   }
-
-  #my ($type, $key, $val) = unpack("nL/aL/a", $data);
-  #print "TYPE: " . $type . "\n";
-  #++$i;
-  #print "$i key='$key' data='$val'\n"; #if not $i % 1000;
 }
 
