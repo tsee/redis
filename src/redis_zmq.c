7bdb5fa915b1309d5f3c13fa39bdd47293b88506
Steffen Mueller <smueller@cpan.org>
2012-06-28 07:39:41 +0200

    Refactor 0MQ expiration message code
    Mostly just shuffle the message sending/object dumping code
    out of the main redis_zmq sources as a first step
    towards cleaning up the terrible mess and to separate concerns.

--- src/redis_zmq.c
+++ src/redis_zmq.c
@@ -34,174 +34,8 @@ static inline long long redis_zmq_expire_loop_expire_time() {
     );
 }
 
-/* write raw data to rio */
-static int rio_write_raw(rio *r, void *p, uint32_t len) {
-    if (r && rioWrite(r, p, len) == 0)
-        return -1;
-    return len;
-}
-
-/* write a length to rio */
-static inline int rio_write_unsigned_32bit(rio *r, uint32_t num) {
-    return rio_write_raw(r, &num, sizeof(uint32_t));
-}
-
-/* write string to rio as string */
-static int rio_write_raw_string(rio *r, unsigned char *s, uint32_t len) {
-    int n, nwritten = 0;
-
-    /* Store verbatim */
-    if ((n = rio_write_unsigned_32bit(r, len)) == -1) return -1;
-    nwritten += n;
-    if (len > 0) {
-        if (rio_write_raw(r, s, len) == -1) return -1;
-        nwritten += len;
-    }
-    return nwritten;
-}
-
-static int rio_write_longlong_as_string(rio *r, long long num) {
-    /* Encode as string */
-    unsigned char buf[32];
-    int enclen;
-    enclen = ll2string((char*)buf, 32, num);
-    redisAssert(enclen < 32);
-    return rio_write_raw_string(r, buf, enclen);
-}
-
-static int rio_write_string_object(rio *r, robj *obj) {
-    if (obj->encoding == REDIS_ENCODING_INT) {
-        return rio_write_longlong_as_string(r, (long long)obj->ptr);
-    } else if (obj->encoding == REDIS_ENCODING_RAW) {
-        return rio_write_raw_string(r, obj->ptr, sdslen(obj->ptr));
-    } else {
-        redisPanic("Not a string encoding we can handle");
-    }
-}
-
-static int rio_write_value(rio *r, robj *o) {
-    int n, nwritten = 0;
-    if (o->type == REDIS_STRING) {
-        /* Save a string value */
-        if ((n = rio_write_string_object(r, o)) == -1) return -1;
-        nwritten += n;
-    } else if (o->type == REDIS_HASH) {
-        /* Save a hash value */
-        if (o->encoding == REDIS_ENCODING_ZIPLIST) {
-            unsigned char *zl;
-            unsigned char *fptr;
-            unsigned int zl_len;
-            unsigned char *vstr = NULL;
-            unsigned int vlen = UINT_MAX;
-            long long vll = LLONG_MAX;
-            int ret;
-
-            zl = o->ptr;
-            zl_len = ziplistLen(zl);
-            rio_write_unsigned_32bit(r, (uint32_t)zl_len);
-
-            fptr = ziplistIndex(zl, 0);
-            /* ziplist element iteration, key and value treated the same -- a number or a string */
-            while (fptr != NULL) {
-                ret = ziplistGet(fptr, &vstr, &vlen, &vll);
-                redisAssert(ret);
-
-                if (vstr) {
-                    if ((n=rio_write_raw_string(r, vstr, vlen)) == -1)
-                        return -1;
-                    nwritten += n;
-                } else {
-                    if ((n=rio_write_longlong_as_string(r, vll)) == -1)
-                        return -1;
-                    nwritten += n;
-                }
-
-                fptr = ziplistNext(zl, fptr);
-            }
-        } else if (o->encoding == REDIS_ENCODING_HT) {
-            dict *d = o->ptr;
-            dictIterator *di = dictGetIterator(d);
-            dictEntry *de;
-
-            if ((n = rio_write_unsigned_32bit(r, dictSize(d)*2)) == -1) return -1;
-            nwritten += n;
-
-            while((de = dictNext(di)) != NULL) {
-                robj *key = dictGetKey(de);
-                robj *val = dictGetVal(de);
-
-                if ((n = rio_write_string_object(r, key)) == -1) return -1;
-                nwritten += n;
-                if ((n = rio_write_string_object(r, val)) == -1) return -1;
-                nwritten += n;
-            }
-            dictReleaseIterator(di);
-        } else {
-            redisPanic("Unknown hash encoding");
-        }
-    } else {
-        redisPanic("Unknown or unsupported object type");
-    }
-    return nwritten;
-}
-
-
-static int zeromqSend(char *str, size_t len, int flags, char *on_error) {
-    int rc;
-    /* size_t bytes; */
-    zmq_msg_t msg;
-    zmq_msg_init_size(&msg, len);
-    memcpy(zmq_msg_data(&msg), str, len);
-    /* bytes = zmq_msg_size(&msg); */
-    rc = zmq_send(redis_zmq_socket, &msg, flags);
-    zmq_msg_close(&msg);
-    if (rc == -1) {
-        redisLog(REDIS_WARNING, on_error, zmq_strerror(zmq_errno()));
-    }
-    return rc;
-}
-
-static void zeromqDumpObject(redisDb *db, robj *key, robj *val) {
-    int rc;
-    /* char event[2]; */
-    uint16_t header[2];
-    rio payload;
-    rio keystr;
-    sds payload_buf = sdsempty();
-    sds keystr_buf = sdsempty();
-
-    rioInitWithBuffer(&payload, payload_buf);
-    rioInitWithBuffer(&keystr, keystr_buf);
-
-    header[0] = (uint16_t)db->id;
-    if (val->type == REDIS_STRING) {
-        header[1] = (uint16_t)REDIS_ZMQ_TYPE_STRING;
-    } else if (val->type == REDIS_HASH) {
-        header[1] = (uint16_t)REDIS_ZMQ_TYPE_HASH;
-    }
-    else {
-        redisPanic("Cannot handle types other than strings and hashes!");
-    }
-
-
-    /*if (val) {
-        rioInitWithBuffer(&payload,sdsempty());
-        redisAssertWithInfo(NULL, key, rio_write_string_object(&payload, key));
-        redisAssertWithInfo(NULL, val, rio_write_value(&payload, val) != -1);
-    }*/
-    redisAssertWithInfo(NULL, key, rio_write_string_object(&keystr, key) != -1);
-    redisAssertWithInfo(NULL, val, rio_write_value(&payload, val) != -1);
-
-    rc = zeromqSend((char *)header, (size_t)4, ZMQ_SNDMORE, "Could not send header: %s");
-    rc = zeromqSend((char *)keystr.io.buffer.ptr, (size_t)sdslen(keystr.io.buffer.ptr), ZMQ_SNDMORE, "Could not send key: %s");
-    rc = zeromqSend((char *)payload.io.buffer.ptr, (size_t)sdslen(payload.io.buffer.ptr), 0, "Could not send payload: %s");
-
-    sdsfree(keystr.io.buffer.ptr);
-    sdsfree(payload.io.buffer.ptr);
-/*    if (rc_db != -1 && rc_event != -1 && rc_key != -1 && rc_val != -1)
-        server.stat_zeromq_events++;
-*/
-}
+/* makes static void zeromqSendObject(redisDb *db, robj *key, robj *val) available */
+#include "redis_zmq_snd_msg.h"
 
 
 /* Attempts to set up context, socket, connection. Don't call unless
@@ -211,7 +45,7 @@ void redis_zmq_init() {
     unsigned int iendpoint;
 
     if (redis_zmq_context == NULL) {
-        redisLog(REDIS_VERBOSE, "Initializing 0MQ for expiry.");
+        redisLog(REDIS_VERBOSE, "Initializing 0MQ for expiration.");
         redis_zmq_context = zmq_init(1);
         if (redis_zmq_context == NULL) {
             redisLog(REDIS_WARNING,"Failed to init 0MQ context");
@@ -248,7 +82,7 @@ void redis_zmq_init() {
 
 
 
-static int getFromHashTable(robj *o, robj *field, robj **value) {
+static int getValueFromHashTable(robj *o, robj *field, robj **value) {
     dictEntry *de;
 
     redisAssert(o->encoding == REDIS_ENCODING_HT);
@@ -318,7 +152,7 @@ static int redis_zmq_check_expire_cycles(redisDb *db, robj *key, robj *o) {
             robj *val2 = NULL;
             robj *name = createStringObject("_expire_cycles", 14);
 
-            getFromHashTable(o, name, &val2);
+            getValueFromHashTable(o, name, &val2);
             if (val2 != NULL)
                 getLongLongFromObject(val2, &nexpirecycles);
 
@@ -364,22 +198,19 @@ static int redis_zmq_check_expire_cycles(redisDb *db, robj *key, robj *o) {
  * Handles only Redis "scalar" string values and hashes!
  */
 /* Returns 0 if the key is not to be deleted after all */
-int dispatchExpiryMessage(redisDb *db, robj *key, int check_expire_cycles) {
-    /* uint32_t rc; */
-    /* size_t msg_len; */
-    /* char *buf; */
+int dispatchExpirationMessage(redisDb *db, robj *key) {
     robj *val;
 
-    /* Abuse endpoint setting to see whether we need to send
-     * expiry messages at all. */
+    /* Abuse endpoint setting to see early whether we need to send
+     * expiration messages at all. */
     if (redis_zmq_num_endpoints == 0)
         return 1;
 
-    val = lookupKey(db, key); /* FIXME this updates expire time... Silly. *gnash teeth* */
+    val = lookupKey(db, key);
 
-    /* We only support dispatching expiry messages on STRING and HASH values for now. */
     /* Technically, it would be vastly more elegant to reuse the actual Redis protocol for
-     * transmitting this information, but for my nefarious purposes, this is good enough.
+     * transmitting this information, but for my nefarious purposes, this is good enough
+     * AND likely more efficient.
      * Also, the Redis functions that encode Redis data structures for output appear
      * to like writing to a global buffer. Probably missed something obvious in my
      * sleep-deprived stupor. */
@@ -390,19 +221,16 @@ int dispatchExpiryMessage(redisDb *db, robj *key, int check_expire_cycles) {
     /* Set up context, socket, and connection. */
     redis_zmq_init();
 
+    /* 0MQ init failed, should have been reported already. Soft fail. */
     if (redis_zmq_socket == NULL)
         return 1;
 
     redisLog(REDIS_DEBUG, "Sending Expire message for %s", (val->type == REDIS_STRING ? "string" : "hash"));
     if (val->type == REDIS_HASH) {
         /* only send messages for hashes because that's what *I* need */
-        zeromqDumpObject(db, key, val);
+        zeromqSendObject(db, key, val);
         removeExpire(db, key);
     }
-
-    /* If expire cycle-checking not necessary, then we're done. */
-    if (check_expire_cycles == 0)
-        return 1;
 
     /* Check whether we need to cycle the key back to the db and do so
      * if necessary. */
@@ -459,7 +287,7 @@ int dispatchExpiryMessage(redisDb *db, robj *key, int check_expire_cycles) {
 
             if ( val != NULL && val->type == REDIS_HASH) {
                 redisLog(REDIS_DEBUG, "Sending Expire message for %s", (val->type == REDIS_STRING ? "string" : "hash"));
-                zeromqDumpObject(db, key, val);
+                zeromqSendObject(db, key, val);
                 removeExpire(db, key);
             }
         } /* end scope */
